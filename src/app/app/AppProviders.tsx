c977f8c5531841662967d67aa66c30dcd7f6d21a
eugene.zhdanov <evgeny1halvin@yandex.ru>
2024-09-07 19:46:34 +0300

    style(app): add normilize css

--- src/app/app/AppProviders.tsx
+++ src/app/app/AppProviders.tsx
@@ -1,9 +1,16 @@
+import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+import { Fragment } from "react/jsx-runtime";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const AppProviders = ({ children }: Props) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <Fragment>
+      <CssBaseline />
+      <BrowserRouter>{children}</BrowserRouter>
+    </Fragment>
+  );
 };
